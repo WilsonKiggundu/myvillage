424edff5dd3d2b8ff38151e767e48a259005a32b
Wilson Kiggundu <wkiggundu@innovationvillage.co.ug>
2020-10-21 13:35:23 +0300

    Updated deploy.sh script to checkout develop branch

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ssh -p 50739 jenkins@test-server.innovationvillage.co.ug <<EOF
  cd /var/www/myvillage-ui
- git checkout master
+ git checkout develop
  git pull
  yarn install
  yarn run build
