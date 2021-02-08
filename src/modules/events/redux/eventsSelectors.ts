4958a8b6a4c79ca78f931f97580154c00aded4a0
Isaac <isaackomeza@gmail.com>
2021-02-08 15:10:40 +0200

    Removing imports and unused variables in events modules

--- src/modules/events/redux/eventsSelectors.ts
+++ src/modules/events/redux/eventsSelectors.ts
@@ -1,9 +1,3 @@
-import { takeLatest } from "redux-saga/effects"
-import {createEvent, fetchEvents} from "./eventsActions"
-import {ADD_EVENT, FETCH_EVENTS} from "./eventsReducer"
-import {IEvent} from "../../../interfaces/IEvent";
-import {isBefore} from "date-fns";
-import {isPast} from "../../../utils/dateHelpers";
 
 export const eventsSelector = (state: any) => state.events
 export const eventSelector = (state: any, id: string) => state.events.data.find((event: any) => event.id === id)
