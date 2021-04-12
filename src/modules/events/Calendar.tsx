83749c4b8032f7eadbfe75cbb27b6f174e603f06
Wilson Kiggundu <wkiggundu@innovationvillage.co.ug>
2021-04-12 10:44:42 +0300

    Added Calendar

---
+++ src/modules/events/Calendar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid"
+
+const Calendar = () => {
+    return (
+        <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth" />
+    )
+}
+
+export default Calendar
