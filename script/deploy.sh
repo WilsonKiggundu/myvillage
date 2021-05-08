86404884a430b280fceb55261da7e92ee6860fbf
Wilson Kiggundu <wkiggundu@innovationvillage.co.ug>
2021-05-08 07:32:55 +0300

    updated deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ssh -p 50739 jenkins@test-server.innovationvillage.co.ug <<EOF
- cd /var/www/myvillage-ui
- git checkout develop
+ssh -p 50739 jenkins@prod-server.innovationvillage.co.ug <<EOF
+ cd /var/www/my-village
+ git checkout master
  git pull
  yarn install
  yarn run build
